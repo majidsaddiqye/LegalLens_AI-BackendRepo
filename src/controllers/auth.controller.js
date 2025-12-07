3f34de42c5dea861c77bc3c3207ef58c9353cc0e
Abdul Majid <majidsaddiqye@gmail.com>
2025-12-07 23:42:22 +0500

    LoginUser Created

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,8 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 async function registerUser(req, res) {
-    console.log('Request Received at Controller:', req.body);
   try {
     const {
       fullName: { firstName, lastName },
@@ -35,6 +35,34 @@ async function registerUser(req, res) {
   }
 }
 
+async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.cookie("token", token);
+
+    return res.status(200).json({ message: "Login successful", user: user });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+
 module.exports = {
   registerUser,
+  loginUser,
 };
